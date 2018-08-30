02eb909b028f0f1f367ea2dd2b6ad55fbfdb2a53
Tabbatha <TabbathaRenea@gmail.com>
2018-08-30 16:27:22 -0400

    fixed image paths in data file

--- js/data.js
+++ js/data.js
@@ -6,28 +6,28 @@ const data = [
     equipProvide: 'Provided',
     equipModel: '21.5 inch iMac',
     equipText: 'Some quick example text to build on the card title and make up the bulk of the cards content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sed natus cupiditate reiciendis facilis odio neque a impedit mollitia! Laudantium nostrum ipsa modi aliquid perspiciatis earum repellendus excepturi atque et?',
-    equipImgSrc: '../images/misc/imac.png'
+    equipImgSrc: 'images/misc/imac.png'
   },
   {
     equipType: 'Camera',
     equipProvide: 'Not Provided',
     equipModel: 'Professional Grade DSLR',
     equipText: 'Some quick example text to build on the card title and make up the bulk of the cards content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sed natus cupiditate reiciendis facilis odio neque a impedit mollitia! Laudantium nostrum ipsa modi aliquid perspiciatis earum repellendus excepturi atque et?',
-    equipImgSrc: '../images/misc/80d.jpg'
+    equipImgSrc: 'images/misc/80d.jpg'
   },
   {
     equipType: 'Lens',
     equipProvide: 'Not Provided',
     equipModel: 'Zoom lens',
     equipText: 'Some quick example text to build on the card title and make up the bulk of the cards content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sed natus cupiditate reiciendis facilis odio neque a impedit mollitia! Laudantium nostrum ipsa modi aliquid perspiciatis earum repellendus excepturi atque et?',
-    equipImgSrc: '../images/misc/70200.jpg'
+    equipImgSrc: 'images/misc/70200.jpg'
   },
   {
     equipType: 'Studio Lights',
     equipProvide: 'Provided',
     equipModel: 'Interfit',
     equipText: 'Some quick example text to build on the card title and make up the bulk of the cards content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sed natus cupiditate reiciendis facilis odio neque a impedit mollitia! Laudantium nostrum ipsa modi aliquid perspiciatis earum repellendus excepturi atque et?',
-    equipImgSrc: '../images/misc/monolight.jpg'
+    equipImgSrc: 'images/misc/monolight.jpg'
   }
  ]
 ]
