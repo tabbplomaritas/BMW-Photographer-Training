0831e98e93ba968236b5eb2d2d191204af305741
tabbatharenea@gmail.com <tabbatharenea@gmail.com>
2018-09-06 10:58:09 -0400

    added navbar collapse functionality to each nav link, made equipment popup div scrollable if text exceeds div length

--- js/data.js
+++ js/data.js
@@ -19,7 +19,7 @@ const data = [
     equipType: 'Lens',
     equipProvide: 'Not Provided',
     equipModel: 'Zoom lens',
-    equipText: 'Some quick example text to build on the card title and make up the bulk of the cards content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sed natus cupiditate reiciendis facilis odio neque a impedit mollitia! Laudantium nostrum ipsa modi aliquid perspiciatis earum repellendus excepturi atque et?',
+    equipText: 'Zoom lens required. Swapping between two prime lens all day long is time consuming and as the basement studio can be quite humid at times, you don’t want to be doing that to your gear. For full frame sensor cameras, you’ll want something that goes at least as wide as 24mm and as telephoto as 70mm. A wider focal length is needed to view the full interior of the vehicle, while a slightly more telephoto length is needed to view the exterior of the car without distortion. The 24-105mm f/4.0 L has been used previously with the Canon 6D I. L series lenses are not necessary required.',
     equipImgSrc: 'images/misc/70200.jpg'
   },
   {
